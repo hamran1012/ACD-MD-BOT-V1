df5c3e4d8bef02aa1fe431c3dae15ab89c5e3e77
hamran1012 <harmandeepsingh20234@gmail.com>
2024-12-14 02:30:45 +0400

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "LEV2UabC#UhyVG_nbUPfZJ7LG3rW6apefVGkWyGG5LL2xA7QYQz8",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Qp22bZ1/IMG-20241129-WA0428.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
-SUDO_NB: process.env.SUDO_NB || "94742398003",
+SUDO_NB: process.env.SUDO_NB || "9475990031",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
